771406b280076d339bb5ff28f710896d18782044
shreya <shreya04kulkarni@gmail.com>
2024-11-14 12:21:46 +0530

    hi

---
+++ JavaMavenExample/src/main/java/com/app/demo/JavaMavenExample/App.java
@@ -0,0 +1,10 @@
+package com.app.demo.JavaMavenExample;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Shreya- 22BD1A051N!");
+    }
+}
